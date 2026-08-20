08418e0de1e7507896ef50755ce3f6758412b771
vitorcostalara-crypto <vitor.costa.lara@escola.pr.gov.br>
2026-08-20 12:18:25 -0300

    ok

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 //Botão curtida
 
-document.addEventListener("DOMContentLoaded", () {
+document.addEventListener("DOMContentLoaded",)()=>{
+const likeBtn = document.querySelector(".lestf-actions .action-btn:first-child");
+if (!likeBtn)return;{
+costlikeSvg=likeBtn.querySelector("svg");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//coração
+funcion applyLikedsle(){
+likeSvg.style.fill=
+
+
+
+
 
 }
+    
+
+
