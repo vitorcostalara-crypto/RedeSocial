3d963018dd105a37b60b763a256c6c112b9a0fdb
vitorcostalara-crypto <vitor.costa.lara@escola.pr.gov.br>
2026-09-10 11:56:45 -0300

    ok

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-//Botão curtida
+//Botao curtidas
+document.addEventListener("DOMContentLoaded", () =>{
+const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+if (!likeBtn) return;
+const likeSvg = likeBtn.querySelector("svg");
 
-document.addEventListener("DOMContentLoaded",)()=>{
-const likeBtn = document.querySelector(".lestf-actions .action-btn:first-child");
-if (!likeBtn)return;{
-costlikeSvg=likeBtn.querySelector("svg");
+
+//localiza o contador
+
+let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+=== Node.TEXT_NODE && node.textContent.trim() !== ""
+);
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+textNode.textContent = `0`;
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 //coração
-funcion applyLikedsle(){
-likeSvg.style.fill=
+function applyLikedStyle (){
+likeSvg.style.fill = "#ef4444";
+likeSvg.style.stroke = "#ef4444";
+likeSvg.style.color = "#ef4444";
 
 
-
-
-
+//efeito curtida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout(() => (likeSvg.style.transform = "scale(1)")150);
 }
     
 
